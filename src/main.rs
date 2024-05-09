7862fbebd67ef242a5c21b59926eb4e3e458140f
Aman <asingh4@k12.com>
2024-05-09 17:56:16 +0530

    add basic server implementation

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server {
+    address: String,
+}
+
+impl Server{
+    fn new(address: String) -> Self {
+        Self{
+            address
+        }
+    }
+
+    fn run(self) {
+        println!("Listining on {}", self.address)
+     }
+}
