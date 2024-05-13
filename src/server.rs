0bd0f59603d0f931a496ba4a8d8d6dfcd3127523
Vivek Singh Rathoure <rathoreviveksingh960@gmail.com>
2024-05-13 08:00:08 +0530

    setup tcp connection

---
+++ src/server.rs
@@ -0,0 +1,37 @@
+use std::net::TcpListener;
+use std::io::Read;
+pub struct Server {
+    address: String,
+}
+
+impl Server{
+    pub fn new(address: String) -> Self {
+        Self{
+            address
+        }
+    }
+
+    pub fn run(self) {
+        println!("Listining on {}", self.address);
+        let listener = TcpListener::bind(&self.address).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _address)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                        }
+                        Err(e) => println!("Failed to read from the connection: {}", e)
+                    }
+                }
+                Err(e) => {
+                    println!("Error {}", e);
+                }
+            }
+        
+        } 
+    }
+}
