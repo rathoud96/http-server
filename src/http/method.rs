f8158f0627d00e26f2921fc1909d90e0f7be04a3
Vivek Singh Rathoure <rathoreviveksingh960@gmail.com>
2024-05-13 07:59:46 +0530

    setup modules for http request and methods

---
+++ src/http/method.rs
@@ -0,0 +1,11 @@
+pub enum Method {
+    GET,
+    PUT,
+    POST,
+    DELETE,
+    HEAD,
+    PATCH,
+    OPTIONS,
+    CONNECT,
+    TRACE
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
+use http::Method;
+use http::Request;
+use server::Server;
+
+mod http;
+mod server;
+
 fn main() {
+    let get = Method::GET;
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    address: String,
-}
-
-impl Server{
-    fn new(address: String) -> Self {
-        Self{
-            address
-        }
-    }
-
-    fn run(self) {
-        println!("Listining on {}", self.address)
-     }
-}
